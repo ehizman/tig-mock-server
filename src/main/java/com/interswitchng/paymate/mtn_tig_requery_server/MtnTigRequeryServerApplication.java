ecec3bcd6303a65087e288b22cc6787395070763
Ehis Edemakhiota <edemaehiz@gmail.com>
2024-07-16 02:05:13 +0100

    first commit

---
+++ src/main/java/com/interswitchng/paymate/mtn_tig_requery_server/MtnTigRequeryServerApplication.java
@@ -0,0 +1,13 @@
+package com.interswitchng.paymate.mtn_tig_requery_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtnTigRequeryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtnTigRequeryServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/interswitchng/paymate/mtn_tig_requery_server/MtnTigRequeryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interswitchng.paymate.mtn_tig_requery_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtnTigRequeryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
